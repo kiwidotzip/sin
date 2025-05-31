de2351eb18abc994fb4e1f9b4060d143716a176b
kiwidotzip <snowykiwii@gmail.com>
2025-05-31 09:59:35 +0530

    add jsdocs

--- elements/textparagraph.js
+++ elements/textparagraph.js
@@ -1,13 +1,27 @@
 import { UIWrappedText, UIContainer, CenterConstraint } from "../../Elementa"
 import BaseElement from "./base"
 
+/**
+ * Text paragraph element for displaying text
+ * @class TextParagraphElement
+ * @extends BaseElement
+ */
 export default class TextParagraphElement extends BaseElement {
+    /**
+     * Creates a new TextParagraphElement
+     * @param {string} description The text to display
+     * @param {boolean} centered Whether to center the text
+     */
     constructor(description = "", centered = true) {
         super(0, 0, 0, 0, description, centered, 'textparagraph')
         this.description = description
         this.centered = centered
     }
 
+    /**
+     * Creates and returns the UI component for this text paragraph
+     * @returns {UIContainer} The main text paragraph container
+     */
     create() {
         this.bg = new UIContainer()
             .setWidth((100).percent())
